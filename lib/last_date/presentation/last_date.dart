7a38232e2208ad33a7662f70a85c530c454a8f99
hello-saif <mdsaifuddinnobab5@gmail.com>
2025-01-30 11:32:10 +0600

    lastDate

---
+++ lib/last_date/presentation/last_date.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class LastDate extends StatefulWidget {
+  const LastDate({super.key});
+
+  @override
+  State<LastDate> createState() => _LastDateState();
+}
+
+class _LastDateState extends State<LastDate> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
