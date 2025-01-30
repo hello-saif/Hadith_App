ec54f33018cd2b0a2f6f3761fc1f810225ed3bfd
hello-saif <mdsaifuddinnobab5@gmail.com>
2025-01-30 11:57:46 +0600

    navigation button

---
+++ lib/navigation_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class NavigationButton extends StatefulWidget {
+  const NavigationButton({super.key});
+
+  @override
+  State<NavigationButton> createState() => _NavigationButtonState();
+}
+
+class _NavigationButtonState extends State<NavigationButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
